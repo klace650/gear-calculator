18117da69aaaac8911a599227f31f2efc231e7a1
Klace Koch <koch.klace@gmail.com>
2021-01-20 17:11:01 -0800

    Cog options now only reflect what is present in JSON data

--- public/js/script.js
+++ public/js/script.js
@@ -3,63 +3,58 @@
 // ===============Under Construction======================
 
 // TEST GLOBALS
-let bombo = [];
-
-// Demo
-let dombo = [1,2,3,4,5,6,6,6,7,8];
-let uniqueSet = new Set(dombo);
-let combo = [...uniqueSet];
-console.log(combo, dombo);
-// NOTES:
-// "new Set" only works with hard coded array
-// seems like the set isn't iterating over the bombo array.
-// TODO: test out if this should be its own function
-
+let combo = [];
+let gearObj = [];
 
 function Gear(model, range, combination, type) {
   this.model = model;
   this.range = range;
   this.combination = combination;
   this.type= type;
-  // TODO: test out if pushing to bombo array - from Gear will allow the new set method to iterate through it.  
+  gearObj.push(this);
 }
-
 $.ajax('./gears.json').then(data => {
+  let x = [];
+  let y = [];
   data.forEach(item => {
+    new Gear(item.model, item.range, item.combination, item.type);
     if (item.type === "cassette"){
-      let gear = new Gear(item.model, item.range, item.combination, item.type);
       let a = item.combination
         a.forEach(item => {
-          bombo.push(item);
-          // TODO: it would be ideal to use this function to iterate though the bombo array and not need to push to a global. since i'm hoping to pull this data from a database eventually.
-      })
+          let b = item;
+          x.push(b)
+        })
+    } else if (item.type === 'chainring'){
+      let c = item.combination
+        c.forEach(item => {
+          let d = item;
+          y.push(d);
+        })
     }
+  })
+  let makeSet = new Set(x);
+  let sortSet = [...makeSet];
+  let sortedSet = sortSet.sort();
+  combo.push(sortedSet)
+  sortedSet.forEach(number => {
+    $("#cogs").append(`<option>${number}</option>x`)
   })
 });
 
-console.log(bombo, 'Pushed from AJAX')
+console.log(combo, 'combo options')
 
 // ===============Under Construction======================
 
 // GLOBAL VARIABLES
 
-let cassMin = 10;
-let cassMax = 50;
 let ringMin = 25;
 let ringMax = 52;
 
-cogDrop(cassMin,cassMax);
 ringDrop(ringMin, ringMax);
 
 
 //  FUNCTIONS --------------------------------------------------
 
-// Creates dropdown options dynamically - uses hard coded ranges*
-function cogDrop(a,b){
-  for(var i = a; i < b + 1; i++){
-    $('#cogs').append(`<option>${i}</option>`)
-  }
-}
 function ringDrop(a,b){
   for(var i = a; i < b + 1; i++){
     $('#chainring').append(`<option>${i}</option>`)
